6c3862ab7303311bf122ef54d1992584fcb649c0
Vladymyr Kysenko <kysenko@viewdle.com>
2011-03-11 00:33:42 +0200

    Added reading to simple

--- simple_train.go
+++ simple_train.go
@@ -6,6 +6,11 @@ import (
 
 func main() {
 	const numLayers = 3
+	const desiredError = 0.001
+	const maxEpochs = 500000
+	const epochsBetweenReports = 1000
+
 	ann := fann.CreateStandart(numLayers, []uint32{2, 3, 1})
+	ann.TrainOnFile("xor.data", maxEpochs, epochsBetweenReports, desiredError)
 	ann.Foo()
 }
