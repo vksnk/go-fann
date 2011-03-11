d187c888a606308027e06cf74d52bc0969d5c731
Vladymyr Kysenko <kysenko@viewdle.com>
2011-03-11 02:01:13 +0200

    Added simple_test

--- fann.go
+++ fann.go
@@ -12,6 +12,9 @@ const unsigned int* go2uintArray(unsigned int* arr, int n) {
 */
 import "C"
 import "unsafe"
+
+type FannType C.fann_type
+
 type TrainData struct {
 	object *C.struct_fann_train_data
 }
@@ -29,10 +32,25 @@ func CreateStandart(numLayers uint, layers []uint32) (*Ann) {
 	return &ann
 }
 
+func CreateFromFile(filename string) (*Ann) {
+	var ann Ann
+	cfn := C.CString(filename)
+	defer C.free(unsafe.Pointer(cfn))
+	ann.object = C.fann_create_from_file(cfn)
+	return &ann
+}
+
 func (ann *Ann) TrainOnFile(filename string, maxEpoches uint32, epochBetweenReports uint32, desiredError float32) {
 	cfn := C.CString(filename)
 	defer C.free(unsafe.Pointer(cfn))
 	C.fann_train_on_file(ann.object, cfn, C.uint(maxEpoches), C.uint(epochBetweenReports), C.float(desiredError));
+}
+
+func (ann *Ann) Run(input []FannType) ([]FannType) {
+	c_out := C.fann_run(ann.object, (*C.fann_type)(&input[0]))
+	out := make([]FannType, 1)
+	out[0] = FannType(*c_out)
+	return out
 }
 
 func (ann *Ann) Save(filename string) {
