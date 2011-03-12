67db7f33f15ddda120a72ace742758cefa7b650e
Vladymyr Kysenko <kysenko@viewdle.com>
2011-03-13 00:24:00 +0200

    Momentums added

---
+++ momentums.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fann"
+	"fmt"
+)
+func main() {
+	const num_layers = 3
+	const num_neurons_hidden = 96
+	const desired_error = 0.001
+
+	train_data := fann.ReadTrainFromFile("datasets/robot.train")
+	test_data := fann.ReadTrainFromFile("datasets/robot.test")
+
+	var momentum float32
+	for momentum = 0.0; momentum < 0.7; momentum += 0.1 {
+		fmt.Printf("============= momentum = %f =============\n", momentum)
+
+		ann := fann.CreateStandart(num_layers, []uint32{train_data.GetNumInput(), num_neurons_hidden, train_data.GetNumOutput()})
+		ann.SetTrainingAlgorithm(fann.TRAIN_INCREMENTAL)
+		ann.SetLearningMomentum(momentum)
+		ann.TrainOnData(train_data, 2000, 500, desired_error)
+
+		fmt.Printf("MSE error on train data: %f\n", ann.TestData(train_data))
+		fmt.Printf("MSE error on test data : %f\n", ann.TestData(test_data))
+
+		ann.Destroy()
+	}
+
+	train_data.Destroy()
+	test_data.Destroy()
+}
